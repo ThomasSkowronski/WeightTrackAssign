12379b3952f6c07931edb471ad0efe7e0d238bb0
Tomfoollery <69408960+Tomfoollery@users.noreply.github.com>
2021-04-18 12:33:30 -0400

    Added the different layouts.

--- app/src/main/java/com/example/weighttracker/MainActivity.java
+++ app/src/main/java/com/example/weighttracker/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.weighttracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_scrn);
+    }
+
+    public void screenChange(View v){
+        switch (v.getId()){
+            case (R.id.buttonEntry):
+                setContentView(R.layout.entry_scrn);
+                break;
+            case (R.id.buttonMain):
+                setContentView(R.layout.main_scrn);
+                break;
+            case (R.id.buttonHistory):
+                setContentView(R.layout.hist_scrn);
+                break;
+            case (R.id.buttonSett):
+                setContentView(R.layout.sett_scrn);
+                break;
+
+
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
     }
 }
